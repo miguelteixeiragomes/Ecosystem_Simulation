686786f94c7b544f27a75e3f49fa2434f26a1441
Miguel Gomes <Miguel Gomes>
2016-11-22 22:07:47 +0000

    finished new_position

--- main.c
+++ main.c
@@ -16,31 +16,73 @@ typedef struct {
 
 
 int* new_position(int gen, Eco_element* ecosystem, int i, int j, int R, int C) {
-	bool north, east, south, west;
-	Eco_element elem = ecosystem[(i - 1)*C + j];
-	if (elem.type == ' ')
-		north = true;
-	elem = ecosystem[i*C + j + 1];
-	if (elem.type == ' ')
-		east = true;
-	elem = ecosystem[(i + 1)*C + j];
-	if (elem.type == ' ')
-		south = true;
-	elem = ecosystem[i*C + j - 1];
-	if (elem.type == ' ')
-		west = true;
+	int direction[4] = { 0, 0, 0, 0 }; // north, east, south, west
+	Eco_element elem;
+	int idx;
+	int size = R*C;
 
+	idx = (i - 1)*C + j;
+	if ((idx > -1) && (idx < size)) {
+		elem = ecosystem[idx];
+		if (elem.type == ' ')
+			direction[0]++;
+	}
+	idx = i*C + j + 1;
+	if ((idx > -1) && (idx < size)) {
+		elem = ecosystem[idx];
+		if (elem.type == ' ')
+			direction[1]++;
+	}
+	idx = (i + 1)*C + j;
+	if ((idx > -1) && (idx < size)) {
+		elem = ecosystem[idx];
+		if (elem.type == ' ')
+			direction[2]++;
+	}
+	idx = i*C + j - 1;
+	if ((idx > -1) && (idx < size)) {
+		elem = ecosystem[idx];
+		if (elem.type == ' ')
+			direction[3]++;
+	}
+
+	int pick = (gen + i + j) % (direction[0] + direction[1] + direction[2] + direction[3]);
 	
+	int dir = 0;
+	while (pick > 0) {
+		if (direction[dir] == 1) {
+			pick--;
+			dir++;
+		}
+		else {
+			dir++
+		}
+	}
+	switch (dir) {
+	case 0:
+		int ret[2] = { i - 1, j     };
+		return ret;
+	case 1:
+		int ret[2] = { i    , j + 1 };
+		return ret;
+	case 2:
+		int ret[2] = { i + 1, j     };
+		return ret;
+	case 3:
+		int ret[2] = { i    , j - 1 };
+		return ret;
+	}
 }
 
 
-void pusher(Eco_element* ecosystem, int R, int C) {
+void pusher(int gen, Eco_element* ecosystem, int R, int C) {
 	int i, j;
 	for (i = 0; i < R; i++) {
 		for (j = 0; j < C; j++) {
 			Eco_element elem = ecosystem[i*C + j];
 			if (elem.type == RABBIT) {
-				local_solver_rabbit(ecosystem, elem, R, C, i, j);
+				int *pos = new_position(gen, ecosystem, i, j, R, C);
+
 			}
 		}
 	}
