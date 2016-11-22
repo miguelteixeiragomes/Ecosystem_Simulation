61ddc53f097c8b72a5d9a28ca13eee739d926fe7
Miguel Gomes <miguel.teixeira.gomes@gmail.com>
2016-11-22 18:54:04 +0000

    working on positioning function

--- main.c
+++ main.c
@@ -15,6 +15,38 @@ typedef struct {
 }Eco_element;
 
 
+int* new_position(int gen, Eco_element* ecosystem, int i, int j, int R, int C) {
+	bool north, east, south, west;
+	Eco_element elem = ecosystem[(i - 1)*C + j];
+	if (elem.type == ' ')
+		north = true;
+	elem = ecosystem[i*C + j + 1];
+	if (elem.type == ' ')
+		east = true;
+	elem = ecosystem[(i + 1)*C + j];
+	if (elem.type == ' ')
+		south = true;
+	elem = ecosystem[i*C + j - 1];
+	if (elem.type == ' ')
+		west = true;
+
+	
+}
+
+
+void pusher(Eco_element* ecosystem, int R, int C) {
+	int i, j;
+	for (i = 0; i < R; i++) {
+		for (j = 0; j < C; j++) {
+			Eco_element elem = ecosystem[i*C + j];
+			if (elem.type == RABBIT) {
+				local_solver_rabbit(ecosystem, elem, R, C, i, j);
+			}
+		}
+	}
+}
+
+
 int main(int argc, char *argv[]){
 	int R = 10, C = 10;
 
