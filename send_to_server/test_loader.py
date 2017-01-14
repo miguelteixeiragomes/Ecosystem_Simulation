1cb304082359e3c2a6d44bc90d63c775a0b32363
Miguel Gomes <Miguel Gomes>
2017-01-14 23:59:09 +0000

    dados do meu desktop

--- send_to_server/test_loader.py
+++ send_to_server/test_loader.py
@@ -2,6 +2,7 @@ import os
 import math
 import make_environment
 
+
 if __name__ == '__main__':
 
     os.system("make")
@@ -14,7 +15,8 @@ if __name__ == '__main__':
     RABBIT = 1
     FOX = 1
 
-    N_THREADS = [1, 2, 4, 8, 16, 32]
+    #N_THREADS = [1, 2, 4, 8, 16, 32]
+    N_THREADS = [1, 2, 3, 4, 5, 6, 7, 8]
     BASE_N = 200
 
     DIMS_LIST= [int(BASE_N*math.sqrt(N)) for N in N_THREADS]
@@ -33,12 +35,13 @@ if __name__ == '__main__':
                                                               ROCK,
                                                               RABBIT,
                                                               FOX))
-    print "testing"
+    print("testing")
     for file_name in INPUT_FILES:
         output_file = file_name + "_output"
         f = open(output_file, "w+")
         f.close()
-        print output_file
+        print(output_file)
         for N in N_THREADS:
-            print N
-            os.system("./ecosystem %s %d >> %s" % (file_name, N, output_file))
+            print(N)
+            #os.system("./ecosystem %s %d >> %s" % (file_name, N, output_file))
+            os.system("ecosystem %s %d >> %s" % (file_name, N, output_file))
